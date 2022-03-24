fd0e26feb0d63a45510b89d46ce349781a9b4c30
ильсаф гарифуллин <ilsgarifullin@yandex.ru>
2022-03-24 16:43:19 +0300

    add a view

--- CalculatorMvvm/CalculatorMvvm/App.xaml.cs
+++ CalculatorMvvm/CalculatorMvvm/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CalculatorMvvm.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace CalculatorMvvm
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new CalculatorPage();
         }
 
         protected override void OnStart()
---
+++ CalculatorMvvm/CalculatorMvvm/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace CalculatorMvvm.ViewModel
+{
+    internal class CalculatorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand Add { protected set; get; }
+        public ICommand Minus { protected set; get; }
+        public ICommand Delenie { protected set; get; }
+        public ICommand Umn { protected set; get; }
+    }
+}
